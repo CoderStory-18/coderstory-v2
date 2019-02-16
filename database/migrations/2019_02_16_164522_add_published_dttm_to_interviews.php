88fde701ba0e5efdb59f1782618ca15df3bf2474
Jess Wallace <jess.wallace@outlook.com>
2019-02-16 22:59:37 +0000

    removed )

--- database/migrations/2019_02_16_164522_add_published_dttm_to_interviews.php
+++ database/migrations/2019_02_16_164522_add_published_dttm_to_interviews.php
@@ -14,7 +14,7 @@ class AddPublishedDttmToInterviews extends Migration
     public function up()
     {
         Schema::table('interviews', function (Blueprint $table) {
-            $table->timestamp('published_at')->nullable());	
+            $table->timestamp('published_at')->nullable();	
         });
     }
 
